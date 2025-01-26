6087faf937c443a25b270d8edf067d608a43532f
serena delfino <serenadelfino@Air-di-serena.homenet.telecomitalia.it>
2025-01-26 13:55:36 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function purchaseCourse(courseName, price) {
+  alert(`You are about to purchase the course: ${courseName} for $${price}.`);
+  // Add your payment integration here, such as PayPal or Stripe.
+  console.log(`Redirecting to payment for ${courseName}...`);
+}
