35e4a05ee0b7693da8aac4d601f0facbd11a0ba3
Riccardo Massa <riccardo@Mac.homenet.telecomitalia.it>
2025-03-21 23:23:27 +0100

    adding initial website files

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Grazie per averci contattato! Ti risponderemo al più presto.');
+});
